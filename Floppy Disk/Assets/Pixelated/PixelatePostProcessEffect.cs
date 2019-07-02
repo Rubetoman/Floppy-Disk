59151c0d9d791083f6dc07c0b241b89b017faec9
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-07-02 13:01:05 +0200

    Added script for new filter

---
+++ Floppy Disk/Assets/Pixelated/PixelatePostProcessEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelatePostProcessEffect : MonoBehaviour
+{
+    public Material effectMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, effectMaterial);
+    }
+}
